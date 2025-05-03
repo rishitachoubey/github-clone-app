0c8a8b8959aee37c5f33e94daa22093b6e28c26a
Rishita Choubey <rishitachoubey@Rishitas-MacBook-Pro.local>
2025-05-03 15:26:03 +0530

    implement fetch repositories

---
+++ src/queries/getRepositories.js
@@ -0,0 +1,39 @@
+import { gql } from '@apollo/client';
+
+export const GET_REPOSITORIES = gql`
+  query {
+    viewer {
+      repositories(first: 10, orderBy: {field: UPDATED_AT, direction: DESC}) {
+        nodes {
+          id
+          name
+          description
+          stargazerCount
+          url
+        }
+      }
+    }
+  }
+`;
+export const GET_REPOSITORIES_WITH_PRS = gql`
+  query {
+    viewer {
+      repositories(first: 10, orderBy: {field: UPDATED_AT, direction: DESC}) {
+        nodes {
+          id
+          name
+          description
+          pullRequests(first: 10, orderBy: {field: CREATED_AT, direction: DESC}) {
+            nodes {
+              id
+              title
+              state
+              url
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
